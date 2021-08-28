68b193c4232d9f5fdb91ec605878e023358485a1
Tomer <Tomer@7470Dell>
2021-08-28 19:55:03 +0300

    edits css

--- SolProject3/launch.bat
+++ SolProject3/launch.bat
@@ -1,1 +1,1 @@
-java --module-path "C:\Program Files\Java\javafx-sdk-16\lib" --add-modules=javafx.controls,javafx.fxml,javafx.media -jar project3.jar
+java --module-path "C:\JavaFX\javafx-sdk-16\lib" --add-modules=javafx.controls,javafx.fxml,javafx.media -jar project3.jar
